141a1e28107cbd3ef2b5477f075a085f1b66fc70
Raian Satyro <raiansatyro45@gmail.com>
2021-10-19 18:16:40 -0300

    Feito algumas alterações no codigo de classes

--- src/classes/pokemon.py
+++ src/classes/pokemon.py
@@ -1,13 +1,13 @@
 import random
 
 class Pokemon:
-    def __init__(self, id, name, type, sex) -> None:
+    def __init__(self, id, name, type, sex = None):
         self.__id = int(id)
         self.__name = str(name)
         self.__type = str(type)
         self.__sex = str(sex)
         
-
+    
     @property
     def id(self):
         return self.__id
@@ -24,11 +24,19 @@ class Pokemon:
     def sex(self):
         return self.__sex
     
+    @sex.setter
+    def sex(self, sex):
+        self.__sex = sex
     
-    def definedSex(self):
-        if random(1, 2) == 1:
-            self.__sex = 'Macho'
-            return self.__sex
-        else: 
-            self.__sex = 'Femea'
-            return self.__sex
+    
+    #def definedSex(self):
+        #numero = random.randint(1, 2)
+        #if numero == 1:
+            #print(1)
+            #self.__sex = 'Macho'
+            #return self.__sex
+        #else: 
+            #print(2)
+            #self.__sex = 'Femea'
+            #return self.__sex
+        
