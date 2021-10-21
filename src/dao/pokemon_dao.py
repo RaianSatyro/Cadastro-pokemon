f0ffcd834b393a5b1d51517fb93d2205aed6b739
Leonan Barbosa <leonan_nan@hotmail.com>
2021-10-21 19:24:25 -0300

    Criado o código pokemon dao

---
+++ src/dao/pokemon_dao.py
@@ -0,0 +1,17 @@
+class PokemonDAO():
+    def init(self, instancia_poke):
+        self._pokemon_json = {}
+        self.preenche_valores_json(instancia_poke)
+
+    def preenche_valores_json(self, instancia):
+        self._pokemon_json['id'] = instancia.id
+        self._pokemon_json['name'] = instancia.name
+        self._pokemon_json['Type'] = instancia.type
+        self._pokemon_json['sex'] = instancia.sex
+
+    def to_json(self):
+        return self._pokemon_json
+
+    def str(self):
+        return str(self._pokemon_json)
+#Pokemon
